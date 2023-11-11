2f1c0e8486d087bac3c4c6b181e321ab579e9dc7
Tornike512 <Dht5152@gmail.com>
2023-11-11 23:15:46 +0400

    Added background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
+import { Background } from "./Components/Background/background";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Background>
+        <h1>texts</h1>
+      </Background>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Background/background.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import BackgroundImage from "./colorful-mountains-night-minimal.jpg";
+
+export function Background(props) {
+  return (
+    <div className="fixed w-full h-full overflow-hidden left-0 top-0">
+      <img
+        className="w-full h-full object-cover"
+        src={BackgroundImage}
+        alt="Mountain Photo"
+      />
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-white">
+        {props.children}
+      </div>
+    </div>
+  );
+}
