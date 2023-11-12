46d53f76a2cec80d512bc20127b457318b74a155
Tornike512 <Dht5152@gmail.com>
2023-11-12 13:53:12 +0400

    Added functions and map

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import { Background } from "./Components/Background/background";
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [error, setError] = useState("");
+  const [value, setValue] = useState(20);
 
   async function getQuotes() {
     try {
-      const response = await axios.get("https://api.quotable.io/quotes?limit=");
+      const response = await axios.get(
+        `https://api.quotable.io/quotes?limit=${getRandomQuote(value)}`
+      );
       setQuotes(response.data.results);
       console.log(response.data.results);
     } catch (error) {
@@ -21,6 +24,10 @@ function App() {
     getQuotes();
   }, []);
 
+  function getRandomQuote(value) {
+    return Math.floor(Math.random() * value);
+  }
+
   if (error) {
     return <h1>Something went wrong</h1>;
   }
@@ -30,6 +37,8 @@ function App() {
       <Background>
         <div className="flex justify-center flex-col w-[600px] text-xl">
           <button
+            value={value}
+            onClick={() => getRandomQuote(20)}
             className="w-[62px] pl-9 pr-[5.25rem] py-3 rounded-lg border-[none]
            bg-[#89cff0] shadow-[2px_1px_4px_#000000]  absolute top-[10rem] left-[36.5rem]"
           >
@@ -42,7 +51,14 @@ function App() {
               </p>
             );
           })}
-          <h1 className="mt-6"> -Charles Dickens</h1>
+          {quotes.map((quote) => {
+            return (
+              <h1 key={quote.id} className="mt-6">
+                {" "}
+                {quote.author}
+              </h1>
+            );
+          })}
         </div>
       </Background>
     </>
